b3d77c3be3ee53afd7030a12677a845b1c76e8ab
Technonmad <loggrim113@gmail.com>
2021-10-24 11:56:38 +0300

    Added multithreading

--- CPP_Matrix/CPP_Matrix/CPP_Matrix.cpp
+++ CPP_Matrix/CPP_Matrix/CPP_Matrix.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <chrono>
 
 using namespace std;
 
@@ -11,7 +13,7 @@ void fillMatrix(double** matrix)
     {
         for (int j = 0; j < matrix_size; j++)
         {
-            matrix[i][j] = (double)rand();
+            matrix[i][j] = (double)(rand() / 100);
         }
     }
 }
@@ -43,18 +45,104 @@ void MultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatri
     }
 }
 
+void ParallelMultiplyMatrix(double** matrixOne, double** matrixTwo, double** resultMatrix)
+{
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < matrix_size; i++)
+    {
+        for (int j = 0; j < matrix_size; j++)
+        {
+            for (int k = 0; k < matrix_size; k++)
+            {
+                resultMatrix[i][j] += matrixOne[i][k] * matrixTwo[k][j];
+            }
+        }
+    }
+}
+
+double shpoor(double** matrix)
+{
+    double result = 0;
+    for (int i = 0; i < matrix_size; i++)
+    {
+        result += matrix[i][i];
+    }
+
+    return result;
+}
+
 int main()
 {
+    cout << "********************************************************************" << endl;
     cout << "Input matrix size: ";
     cin >> matrix_size;
+
     double** matrix1 = new double* [matrix_size];
     for (int i = 0; i < matrix_size; i++)
     {
         matrix1[i] = new double[matrix_size];
     }
+
+    double** matrix2 = new double* [matrix_size];
+    for (int i = 0; i < matrix_size; i++)
+    {
+        matrix2[i] = new double[matrix_size];
+    }
+
+    double** resultMatrix = new double* [matrix_size];
+    for (int i = 0; i < matrix_size; i++)
+    {
+        resultMatrix[i] = new double[matrix_size];
+    }
+
+    for (int i = 0; i < matrix_size; i++)
+    {
+        for (int j = 0; j < matrix_size; j++)
+        {
+            resultMatrix[i][j] = 0;
+        }
+    }
+    
     fillMatrix(matrix1);
-    printMatrix(matrix1);
+    fillMatrix(matrix2);
+
+    if (matrix_size <= 5) {
+        printMatrix(matrix1);
+        cout << endl;
+
+        printMatrix(matrix2);
+        cout << endl;
+    }
+
+    auto start1 = chrono::high_resolution_clock::now();
+    MultiplyMatrix(matrix1, matrix2, resultMatrix);
+    auto end1 = chrono::high_resolution_clock::now();
+    chrono::duration<float> elapsed_time = end1 - start1;
+    cout << "One thread:" << endl;
+    cout << "\tElapsed time: " << elapsed_time.count() << endl;
+
+    if (matrix_size <= 5) printMatrix(resultMatrix);
+    else cout << "Shpoor: " << (long)shpoor(resultMatrix) << endl;
+
+    for (int i = 0; i < matrix_size; i++)
+    {
+        for (int j = 0; j < matrix_size; j++)
+        {
+            resultMatrix[i][j] = 0;
+        }
+    }
+
+    auto start2 = chrono::high_resolution_clock::now();
+    ParallelMultiplyMatrix(matrix1, matrix2, resultMatrix);
+    auto end2 = chrono::high_resolution_clock::now();
+    chrono::duration<float> elapsed_time2 = end2 - start2;
+    cout << "4 threads:" << endl;
+    cout << "\tElapsed time: " << elapsed_time2.count() << endl;
+
+    if (matrix_size <= 5) printMatrix(resultMatrix);
+    else cout << "Shpoor: " << (long)shpoor(resultMatrix) << endl;
     cout << endl;
+
     return main();
 }
 
